fe0864a39ed9838de069f783d4257b4616d5e31a
DI0IK <76131420+DI0IK@users.noreply.github.com>
2025-11-05 18:23:30 +0000

    Add noindex meta tag to layout
    Add robots meta tag to prevent indexing

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name='robots' content='noindex,follow' />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
